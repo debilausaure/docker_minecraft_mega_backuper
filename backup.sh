9967c05763f48a75ab445a53a6bca6ac77ac5607
Florian Vanhems <flovhs59@gmail.com>
2024-10-07 20:20:50 +0200

    Fix backup.tar.gz path

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ mcrcon -s -H minecraft_server -p rconpassword "say Backing up the world..." save
 
 cd mountpoint
 # Creating a compressed tar of the world
-tar -czf backup.tar.gz banned-ips.json banned-players.json config eula.txt fabric-server-launcher.properties logs ops.json server.properties usercache.json whitelist.json world
+tar -czf ../backup.tar.gz banned-ips.json banned-players.json config eula.txt fabric-server-launcher.properties logs ops.json server.properties usercache.json whitelist.json world
 
 # Remove included logs
 rm -rf logs/*.log.gz
